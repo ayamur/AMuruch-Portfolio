0901383fa3f0acaab2456b429661db21feea0689
AyaMu <ayalamuruch@gmail.com>
2023-01-29 18:11:09 -0500

    fill initial information and photo in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 function Home() {
   return (
-    <h1>Home Page!</h1>
+    <>
+    <h1>Ayala Murúch</h1>
+    <h2>Software Engineer</h2>
+    <img id='home' src='dogexe.png' alt="placeholder" />
+    <h6>Made with 🧠 + 🫀 + 🩸 + 🥲 + ⛤ by <a href="https://github.com/ayamurg">Ayala Murúch</a></h6>
+    </>
   )
 }
 
