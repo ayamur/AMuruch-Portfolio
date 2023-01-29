640048c88882818bb54ab3313a31e439267273e1
AyaMu <ayalamuruch@gmail.com>
2023-01-29 18:26:47 -0500

    update aAbout blurb

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,14 +5,14 @@ function About() {
     <h2>Software Engineer with a Background in Design, Non-Profits, and Game Testing Passionate about Coding, Cryptids, and Uplifting Others! </h2>
     <h2>--put in lots of versions of hello in various languages here--</h2>
     <img src='itsame_ayalamuruch.jpg' id='itsame' alt='my face' />
-    <h4>I am currently working through General Assembly’s software engineer immersion program. This program is fast-paced and intense, but participating is really allowing me to hone in on my strengths and use them to gain a variety of new skills and growth in a supportive environment.
-      <br />
+    <p>I am currently working through General Assembly’s software engineer immersion program. This program is fast-paced and intense, but participating is really allowing me to hone in on my strengths and use them to gain a variety of new skills and growth in a supportive environment.
+      </p><p>
 I have always been passionate about uplifting the moods and spirits of others when possible, with my strong empathy and compassion traits. While I am creative and enthusiastic, this program is allowing me to pursue other avenues of thinking and processing information with a more logical approach. Even most recently I learned a valuable lesson about the importance of celebrating small victories, personal and professional, in the midst of larger endeavors.
-<br />  
+</p><p>
 My past is varied, and has allowed me the opportunity to be flexible and grow in a variety of situations, from teaching English overseas to all age groups, to learning what it takes to develop and maintain a small local nonprofit as a PR and graphics assistant. Even while I have focused on adapting and growing in my personal life, that did not stop me from finding ways to do what I enjoy, by helping others when the opportunity to do so arises. 
-<br />
+</p><p>
 My biggest goal is to continue to pursue my own career path in the field of tech that will allow me to continue to pursue and understand different languages, but at the same time be a place where I can really help others, including those I work with, with my strengths of multiple soft-skills and genuine care and concern for others.   
-</h4>
+</p>
     </>
   )
 }
