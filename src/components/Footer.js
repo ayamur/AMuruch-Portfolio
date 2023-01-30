08cdf8dfe843ce648f83f48f39e64c55f472ad60
AyaMu <ayalamuruch@gmail.com>
2023-01-30 12:42:52 -0500

    update resume with resume embed and button

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <div>
+      <>
+    <h1>Ayala Murúch</h1>
+    <h2>Software Engineer</h2>
+    <img id='home' src='dogexe.png' alt="placeholder" />
+    <h6>Made with 🧠 + 🫀 + 🩸 + 🥲 + ⛤ by <a href="https://github.com/ayamur">Ayala Murúch</a></h6>
+    </>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,14 @@
+// import MuruchResume.pdf from '../../public/MuruchResume.pdf';
+
 function Resume() {
   return (
+    <>
     <h1>Resume Page!</h1>
+    <embed src='MuruchResume.pdf' type='application/pdf' id='resume' />
+    <a href='MuruchResume.pdf' download='MuruchResume.pdf'>
+      <button>Download Resume</button>
+      </a>
+    </>
   )
 }
 
