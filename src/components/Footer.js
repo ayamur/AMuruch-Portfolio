fc9a41ec30f1f3a42d67896e7409ed3b2259250f
AyaMu <ayalamuruch@gmail.com>
2023-01-30 18:10:54 -0500

    add family motto to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,9 @@ function Footer() {
     <h2>Software Engineer</h2>
     <img id='home' src='dogexe.png' alt="placeholder" />
     <h6>Made with 🧠 + 🫀 + 🩸 + 🥲 + ⛤ by <a href="https://github.com/ayamur">Ayala Murúch</a></h6>
+    <div>
+    Libenter animos illorum qui nos frenare conentur devoramus.
+    </div>
     </>
     </div>
   )
