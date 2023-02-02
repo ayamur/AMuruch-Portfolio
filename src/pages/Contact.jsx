c5ffd8dbb0e96e1599e7c8b736546f787c32646d
AyaMu <ayalamuruch@gmail.com>
2023-02-01 20:06:25 -0500

    add links to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,16 @@
 function Contact() {
   return (
     <>
-    <h1>Contact Page!</h1>
-    <img src='github-mark-white.png' id="githublogo" className='contacticons' alt='github logo' />
-    <img src='linkedinwhite.png' id="linkedinlogo" className='contacticons' alt='linkedin logo' />
-    <img src='mailwhite.png' id="mailicon" className='contacticons' alt='mail icon' />
+      <h1>Feel Free to Contact Me!</h1>
+      <a href='https://github.com/ayamur'>
+        <img src='github-mark-white.png' id="githublogo" className='contacticons' alt='github logo' />
+      </a>
+      <a href='https://www.linkedin.com/in/ayalamuruch/'>
+        <img src='linkedinwhite.png' id="linkedinlogo" className='contacticons' alt='linkedin logo' />
+      </a>
+      <a href='mailto:ayalamuruch@gmail.com'>
+        <img src='mailwhite.png' id="mailicon" className='contacticons' alt='mail icon' />
+      </a>
     </>
   )
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,11 +4,11 @@ function Resume() {
   return (
     <>
       <h1>Resume Page!</h1>
-      <embed src='MuruchResume.pdf' type='application/pdf' id='resume' className='navbarbtn'/>
+      <embed src='MuruchResume.pdf' type='application/pdf' id='resume' className='navbarbtn' />
       <br />
-        <a href='MuruchResume.pdf' download='MuruchResume.pdf' id="downloadbtn" className='navbarbtn'>
-      <button>Download Resume</button>
-    </a>
+      <a href='MuruchResume.pdf' download='MuruchResume.pdf' id="downloadbtn" className='navbarbtn'>
+        <button>Download Resume</button>
+      </a>
     </>
   )
 }
