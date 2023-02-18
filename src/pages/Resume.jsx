432b3d4bd5f805d7d8b2eb62c25b5401a5b8a340
AyaMu <ayalamuruch@gmail.com>
2023-02-18 18:45:30 -0500

    add flair and change wording for resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,13 @@
 function Resume() {
   return (
     <>
+    <h2>~View/Download My 1st Quarter 2023 Resume~</h2>
       <embed src='MuruchResume.pdf' type='application/pdf' id='resume' className='navbarbtn' />
       <br />
       <a href='MuruchResume.pdf' download='MuruchResume.pdf' id="downloadbtn" className='navbarbtn'>
-        <button>Download Resume</button>
-      <h1>Feel Free to Request References!</h1>
+        <button id='resume-btn'>Download Resume</button>
       </a>
+      <h2 id='references'>Feel Free to Request References!</h2>
     </>
   )
 }
