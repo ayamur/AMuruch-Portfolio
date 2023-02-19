e487f6dbd5aa13683b1aefa87f7e1d26074f8bb2
AyaMu <ayalamuruch@gmail.com>
2023-02-18 23:00:44 -0500

    update looking for in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <div class='home'>
           <h4>Full-Stack Developer / Software Engineer</h4>
           <h3>-|- React -|- CSS -|- HTML -|- JavaScript -|- TypeScript -|- JSON -|- JSX -|- AJAX -|- EJS -|-  MongoDB -|- Mongoose -|- Node -|- NPM -|- MERN Stack -|- TypeScript -|- Sequelize -|- PostGRE SQL -|- </h3>
-          <h4>Currently Looking For: Remote Junior Development Position in an Educational and Creative Environment Where My Ideas,Enthusiasm, and Excitability Can Make a Positive Difference.</h4>
+          <h4>Currently Looking For: A Remote Junior Development Position in an Educational and Creative Environment where my Ideas,Enthusiasm, and Excitability can make a Positive Difference.</h4>
           <img src='/itsame.jpg' id='itsame' alt='my face' />
           <h3 id='hellos'>| Hello | hallo | Përshëndetje | ሀሎ | مرحبا  |Բարեւ | Salam | kaixo | добры дзень | হ্যালো | zdravo | Здравейте | Hola | kumusta | 你好 | Bonghjornu | Ahoj | Hej | saluton | Tere | Hei | Bonjour | Hoi | ola | გამარჯობა | Χαίρετε | નમસ્તે | bonjou | sannu | aloha | שלום | 	नमस्ते | nyob zoo | Helló | Halló | Nnọọ | Halo | Dia dhuit | Ciao | こんにちは | halo | ಹಲೋ | សួស្តី | Slav | салам | ສະບາຍດີ | 	Salve | Sveiki | Здраво | Salama | ഹലോ | tena koutou | नमस्कार | Сайн уу | ဟယ်လို | Moni | سلام | dzień dobry | Olá | Salut | Здравствуйте | talofa | Halò | Lumela | mhoro | හෙලෝ | Ahoj | Hallå | Kamusta | Салом | வணக்கம் | హలో | สวัสดี | Merhaba | Здрастуйте | ہیلو | Salom | Helo | Mholweni | העלא | Pẹlẹ o | Sawubona | Bongu |ਸਤ ਸ੍ਰੀ ਅਕਾਲ | xin chào | Сәлеметсіз бе | 여보세요 |</h3>
         </div>
