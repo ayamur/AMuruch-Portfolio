5d5ebfe236da4d66e3558d64863676218d2b2216
AyaMu <ayalamuruch@gmail.com>
2023-03-07 01:45:40 -0500

    re-organize tech skills

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,24 @@ function Home() {
       <div class='home-all'>
         <div class='home'>
             <h6 id='hellos'>| Hello | hallo | ሀሎ | مرحبا  |Բարեւ | Salam | kaixo | добры дзень | হ্যালো | zdravo | Здравейте | Hola | kumusta | 你好 | Bonghjornu | Ahoj | Hej | saluton | Tere | Hei | Bonjour | Hoi | ola | გამარჯობა | Χαίρετε | નમસ્તે | bonjou | sannu | aloha | שלום | 	नमस्ते | nyob zoo | Helló | Halló | Nnọọ | Halo | Dia dhuit | Ciao | こんにちは | halo | ಹಲೋ | សួស្តី | Slav | салам | ສະບາຍດີ | 	Salve | Sveiki | Здраво | Salama | ഹലോ | tena koutou | नमस्कार | Сайн уу | ဟယ်လို | Moni | سلام | dzień dobry | Olá | Salut | Здравствуйте | talofa | Halò | Lumela | mhoro | හෙලෝ | Ahoj | Hallå | Kamusta | Салом | வணக்கம் | హలో | สวัสดี | Merhaba | Здрастуйте | ہیلو | Salom | Helo | Mholweni | העלא | Pẹlẹ o | Sawubona | Bongu |ਸਤ ਸ੍ਰੀ ਅਕਾਲ | xin chào | Сәлеметсіз бе | 여보세요 | Përshëndetje |</h6>
-            <h3><em>My name is:</em></h3>
+            <h4 className='homedivide'><em>My name is:</em></h4>
           <h1> Ayala Murúch </h1>
-          <h3><em>and I am a :</em></h3>
+          <h4 className='homedivide'><em>and I am a :</em></h4>
           <div className='homeone'>
             <img src='/itsame.jpg' id='itsame' alt='my face' />
             <h2>Software Engineer Passionate about Learning, Uplifting Others, and Everything Paranormal, with Professional Experience in Design, Non-Profits, and Game Testing!</h2>
           </div>
-          <h3><em>with skills in:</em></h3>
-          <h3>Tech React -|- CSS -|- HTML -|- JavaScript -|- TypeScript -|- JSON -|- JSX -|- AJAX -|- EJS -|-  MongoDB -|- Mongoose -|- Node -|- NPM -|- MERN Stack -|- Sequelize -|- PostGRE SQL -|- jQuery -|- Bootstrap -|- Express.js -|- APIs -|- MEN stack -|- JSX -|- SPAs -|- MVC -|- RESTful Routing -|- bit.io -|- GitHub -|- JWTs -|- GoogleOAuth -|-</h3>
-          <h3><em>and I am looking for:</em></h3>
+          <h4 className='homedivide'><em>with skills in:</em></h4>
+          <div className='waphome'>
+          <h3>Frameworks/Libraries:</h3>
+          <div id='expressjs'><img src='https://img.icons8.com/office/30/null/express-js.png' alt='express.js icon'/> <h6> npm, jQuery, React</h6></div></div>
+          <div>
+          <h3>Languages: <div>JavaScript, HTML, CSS, TypeScript, EJS</div></h3></div>
+          <h3>Web Application Stacks: MERN, PERN</h3>
+          <h3>Database/ Backend: PostgreSQL, MongoDB, <div id='expressjs'><img src='https://img.icons8.com/office/30/null/express-js.png' alt='express.js icon'/>(Express.js)  Node.js</h3>
+          <h3>Software Development Platforms: GitHub, </h3>
+          <h3>Additional: RESTful Routing, APIs, Mongoose ODM, JSON, JWT, AJAX, Sequelize, SPA, MVC framework, bit.io, OAuth </h3>
+          <h4 className='homedivide'><em>and I am looking for:</em></h4>
           <div className='hometwo'>
             <h2>A Remote Junior Software or Web Development Position in an Educational and Creative Environment where my Ideas, Enthusiasm, and Excitability can make a Positive Difference!</h2>
             <h6>(...and proof of supernatural phenomena.) </h6>
