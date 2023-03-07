f6bc6bd9497d1a4e3d8b5583133791cb27650ba8
AyaMu <ayalamuruch@gmail.com>
2023-03-07 09:37:10 -0500

    update signature button CSS to fix linking with background image error

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,29 +5,30 @@ function NavBar() {
   return (
     <div className='navdiv'>
       <nav>
+        <Link to='/' >
           <button type='button' id='sigbtn' className='navbarbtn'>
-            <Link to='/' />
           </button>
-          <Link to='/about'>
-            <button type='button' id='aboutbtn' className='navbarbtn' >
-              About
-            </button>
-          </Link>
-          <Link to='/contact'>
-            <button type='button' id='contactbtn' className='navbarbtn'>
-              Contact
-            </button>
-          </Link>
-          <Link to='/projects'>
-            <button type='button' id='projectsbtn' className='navbarbtn'>
-              Projects
-            </button>
-          </Link>
-          <Link to='/resume'>
-            <button type='button' id='resumebtn' className='navbarbtn'>
-              Resume
-            </button>
-          </Link>
+        </Link>
+        <Link to='/about'>
+          <button type='button' id='aboutbtn' className='navbarbtn' >
+            About
+          </button>
+        </Link>
+        <Link to='/contact'>
+          <button type='button' id='contactbtn' className='navbarbtn'>
+            Contact
+          </button>
+        </Link>
+        <Link to='/projects'>
+          <button type='button' id='projectsbtn' className='navbarbtn'>
+            Projects
+          </button>
+        </Link>
+        <Link to='/resume'>
+          <button type='button' id='resumebtn' className='navbarbtn'>
+            Resume
+          </button>
+        </Link>
       </nav>
     </div>
   )
