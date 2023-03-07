085a1280b3fd986319da7223923471bc893556e0
AyaMu <ayalamuruch@gmail.com>
2023-03-07 00:38:45 -0500

    update hello landing sizing, add CSS media query

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,21 @@ function Home() {
     <>
       <div class='home-all'>
         <div class='home'>
-      <h1> Ayala Murúch </h1>
-          <img src='/itsame.jpg' id='itsame' alt='my face' />
-          <h2>Software Engineer Passionate about Learning, Uplifting Others, and Everything Paranormal, with Professional Experience in Design, Non-Profits, and Game Testing! </h2>
+            <h6 id='hellos'>| Hello | hallo | ሀሎ | مرحبا  |Բարեւ | Salam | kaixo | добры дзень | হ্যালো | zdravo | Здравейте | Hola | kumusta | 你好 | Bonghjornu | Ahoj | Hej | saluton | Tere | Hei | Bonjour | Hoi | ola | გამარჯობა | Χαίρετε | નમસ્તે | bonjou | sannu | aloha | שלום | 	नमस्ते | nyob zoo | Helló | Halló | Nnọọ | Halo | Dia dhuit | Ciao | こんにちは | halo | ಹಲೋ | សួស្តី | Slav | салам | ສະບາຍດີ | 	Salve | Sveiki | Здраво | Salama | ഹലോ | tena koutou | नमस्कार | Сайн уу | ဟယ်လို | Moni | سلام | dzień dobry | Olá | Salut | Здравствуйте | talofa | Halò | Lumela | mhoro | හෙලෝ | Ahoj | Hallå | Kamusta | Салом | வணக்கம் | హలో | สวัสดี | Merhaba | Здрастуйте | ہیلو | Salom | Helo | Mholweni | העלא | Pẹlẹ o | Sawubona | Bongu |ਸਤ ਸ੍ਰੀ ਅਕਾਲ | xin chào | Сәлеметсіз бе | 여보세요 | Përshëndetje |</h6>
+            <h3><em>My name is:</em></h3>
+          <h1> Ayala Murúch </h1>
+          <h3><em>and I am a :</em></h3>
+          <div className='homeone'>
+            <img src='/itsame.jpg' id='itsame' alt='my face' />
+            <h2>Software Engineer Passionate about Learning, Uplifting Others, and Everything Paranormal, with Professional Experience in Design, Non-Profits, and Game Testing!</h2>
+          </div>
+          <h3><em>with skills in:</em></h3>
           <h3>Tech React -|- CSS -|- HTML -|- JavaScript -|- TypeScript -|- JSON -|- JSX -|- AJAX -|- EJS -|-  MongoDB -|- Mongoose -|- Node -|- NPM -|- MERN Stack -|- Sequelize -|- PostGRE SQL -|- jQuery -|- Bootstrap -|- Express.js -|- APIs -|- MEN stack -|- JSX -|- SPAs -|- MVC -|- RESTful Routing -|- bit.io -|- GitHub -|- JWTs -|- GoogleOAuth -|-</h3>
-          <h2>Currently Looking For: A Remote Junior Software or Web Development Position in an Educational and Creative Environment where my Ideas, Enthusiasm, and Excitability can make a Positive Difference!</h2>
-          <h3 id='hellos'>| Hello | hallo | ሀሎ | مرحبا  |Բարեւ | Salam | kaixo | добры дзень | হ্যালো | zdravo | Здравейте | Hola | kumusta | 你好 | Bonghjornu | Ahoj | Hej | saluton | Tere | Hei | Bonjour | Hoi | ola | გამარჯობა | Χαίρετε | નમસ્તે | bonjou | sannu | aloha | שלום | 	नमस्ते | nyob zoo | Helló | Halló | Nnọọ | Halo | Dia dhuit | Ciao | こんにちは | halo | ಹಲೋ | សួស្តី | Slav | салам | ສະບາຍດີ | 	Salve | Sveiki | Здраво | Salama | ഹലോ | tena koutou | नमस्कार | Сайн уу | ဟယ်လို | Moni | سلام | dzień dobry | Olá | Salut | Здравствуйте | talofa | Halò | Lumela | mhoro | හෙලෝ | Ahoj | Hallå | Kamusta | Салом | வணக்கம் | హలో | สวัสดี | Merhaba | Здрастуйте | ہیلو | Salom | Helo | Mholweni | העלא | Pẹlẹ o | Sawubona | Bongu |ਸਤ ਸ੍ਰੀ ਅਕਾਲ | xin chào | Сәлеметсіз бе | 여보세요 | Përshëndetje |</h3>
+          <h3><em>and I am looking for:</em></h3>
+          <div className='hometwo'>
+            <h2>A Remote Junior Software or Web Development Position in an Educational and Creative Environment where my Ideas, Enthusiasm, and Excitability can make a Positive Difference!</h2>
+            <h6>(...and proof of supernatural phenomena.) </h6>
+          </div>
         </div>
       </div>
       <a href='#top' class='up' alt='jellyfish and arrow'>
