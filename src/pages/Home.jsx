aa755703305a824fec39abcda521c4fe248f3aef
AyaMu <ayalamuruch@gmail.com>
2023-03-06 23:55:44 -0500

    update intro blurb, edit typo, and modify up arrow sizing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,9 @@ function Home() {
         <div class='home'>
       <h1> Ayala Murúch </h1>
           <img src='/itsame.jpg' id='itsame' alt='my face' />
-          <h2>Software Engineer with a Background in Design, Non-Profits, and Game Testing. Passionate about Coding, Uplifting Others, and Everything Paranormal! </h2>
-          <h3>Tech React -|- CSS -|- HTML -|- JavaScript -|- TypeScript -|- JSON -|- JSX -|- AJAX -|- EJS -|-  MongoDB -|- Mongoose -|- Node -|- NPM -|- MERN Stack -|- TypeScript -|- Sequelize -|- PostGRE SQL -|- jQuery -|- Bootstrap -|- Express.js -|- APIs -|- MEN stack -|- JSX -|- SPAs -|- MVC -|- RESTful Routing -|- bit.io -|- GitHub -|- JWTs -|- GoogleOAuth -|-</h3>
-          <h2>Currently Looking For: A Remote Junior Software or Web Develpoment Position in an Educational and Creative Environment where my Ideas, Enthusiasm, and Excitability can make a Positive Difference!</h2>
+          <h2>Software Engineer Passionate about Learning, Uplifting Others, and Everything Paranormal, with Professional Experience in Design, Non-Profits, and Game Testing! </h2>
+          <h3>Tech React -|- CSS -|- HTML -|- JavaScript -|- TypeScript -|- JSON -|- JSX -|- AJAX -|- EJS -|-  MongoDB -|- Mongoose -|- Node -|- NPM -|- MERN Stack -|- Sequelize -|- PostGRE SQL -|- jQuery -|- Bootstrap -|- Express.js -|- APIs -|- MEN stack -|- JSX -|- SPAs -|- MVC -|- RESTful Routing -|- bit.io -|- GitHub -|- JWTs -|- GoogleOAuth -|-</h3>
+          <h2>Currently Looking For: A Remote Junior Software or Web Development Position in an Educational and Creative Environment where my Ideas, Enthusiasm, and Excitability can make a Positive Difference!</h2>
           <h3 id='hellos'>| Hello | hallo | ሀሎ | مرحبا  |Բարեւ | Salam | kaixo | добры дзень | হ্যালো | zdravo | Здравейте | Hola | kumusta | 你好 | Bonghjornu | Ahoj | Hej | saluton | Tere | Hei | Bonjour | Hoi | ola | გამარჯობა | Χαίρετε | નમસ્તે | bonjou | sannu | aloha | שלום | 	नमस्ते | nyob zoo | Helló | Halló | Nnọọ | Halo | Dia dhuit | Ciao | こんにちは | halo | ಹಲೋ | សួស្តី | Slav | салам | ສະບາຍດີ | 	Salve | Sveiki | Здраво | Salama | ഹലോ | tena koutou | नमस्कार | Сайн уу | ဟယ်လို | Moni | سلام | dzień dobry | Olá | Salut | Здравствуйте | talofa | Halò | Lumela | mhoro | හෙලෝ | Ahoj | Hallå | Kamusta | Салом | வணக்கம் | హలో | สวัสดี | Merhaba | Здрастуйте | ہیلو | Salom | Helo | Mholweni | העלא | Pẹlẹ o | Sawubona | Bongu |ਸਤ ਸ੍ਰੀ ਅਕਾਲ | xin chào | Сәлеметсіз бе | 여보세요 | Përshëndetje |</h3>
         </div>
       </div>
