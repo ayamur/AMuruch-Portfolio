fae8780ddb3bd01be5d8f7942596a0d4768699f2
AyaMu <ayalamuruch@gmail.com>
2023-03-06 20:55:26 -0500

    adjust font style and portrait image size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,11 @@ function Footer(props) {
       <>
         <h6>Made with 🧠 + 🫀 + 🩸 + 🥲 + ⛤ by <a href='https://github.com/ayamur'>Ayala Murúch</a></h6>
         <h6 id='familymotto'>
-          Murúch family motto:
-          <em> "Libenter animos illorum qui nos frenare conentur devoramus."
+        Murúch family motto:
+          <em> "Libenter animos illorum qui nos frenare conentur devoramus." ™
           </em>
         </h6>
+        <h5><a href='mailto:ayalamuruch@gmail.com' className='resume-icon'>ayalamuruch@gmail.com</a></h5>
       </>
     </div>
 
