b07bdfd88436e8f41947756fb892f3e460a48eaa
AyaMu <ayalamuruch@gmail.com>
2023-03-18 20:36:53 -0400

    import and create displays for example images for graphics page

--- src/pages/Graphics.jsx
+++ src/pages/Graphics.jsx
@@ -1,16 +1,15 @@
 function Graphics() {
   return (
     <>
-    <img src='' id='' alt='' />
-    <img src='' id='' alt='' />
-    <img src='' id='' alt='' />
-    <img src='' id='' alt='' />
-    <img src='' id='' alt='' />
-    <img src='' id='' alt='' />
-    <img src='' id='' alt='' />
-    <img src='' id='' alt='' />
-    <img src='' id='' alt='' />
-    <img src='' id='' alt='' />
+    <img src='/graphics/captainmothman_watermark.png' id='captainmothman' className='artexamples' alt='breakdancing captain mothman' />
+    <img src='/graphics/wujia_watermark.png' id='wujia' className='artexamples' alt='wujia flamenco dancing' />
+    <img src='/graphics/universehands_watermark.png' id='hands' className='artexamples' alt='an outer space hand dangling planets' />
+    <img src='/graphics/universehandslove_watermark.png' id='handslove' className='artexamples' alt='an outer space hand with a heart, holding a crescent moon' />
+    <img src='/graphics/universehandsfood_watermark.png' id='handsfood' className='artexamples' alt='outer space hands presenting a plate with crossed utensils' />
+    <img src='/graphics/universehandstravel_watermark.png' id='handstravel' className='artexamples' alt='a space hand presenting a planet with a ufo and an airplane ' />
+    <img src='/graphics/jaceflaretwitchoffline_watermark.png' id='jaceflare' className='artexamples' alt='a penniless jester near an arcade machine' />
+    <img src='/graphics/hunterlogo_watermark.png' id='hunterlogo' className='artexamples' alt='occult influenced logo pertaining to the game New World' />
+    {/* <img src='' id='' alt='' /> */}
     </>
   )
 }
