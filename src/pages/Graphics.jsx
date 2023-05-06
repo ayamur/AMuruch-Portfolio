c3e4b70cd1be4490c70426d221de4bdcc1046f5a
AyaMu <ayalamuruch@gmail.com>
2023-05-06 17:52:58 -0400

    change anchor to react link for functionality

--- src/pages/Graphics.jsx
+++ src/pages/Graphics.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import GraphicsList from '../components/GraphicsList'
 import graphics from '../data/graphics'
 
@@ -13,7 +14,7 @@ const Graphics = () => {
       <a href='#top' className='up' alt='jellyfish and arrow'>
         <img src='/jelly_up.png' alt='jellyfish with up arrow' className='jellyarrow' />
       </a>
-      <h2>For further information about contracts, freelance projects, examples, or any other inquiries, <a href='/contact'>click here for ways to contact me</a></h2>
+      <h2>For further information about contracts, freelance projects, examples, or any other inquiries, <Link to='/contact'>click here for ways to contact me</Link></h2>
     </>
   )
 }
